e7a408ec8ff90e279add9a4157000e6177a32f56
Sanket Mhetre <sanketmhetre66@gmail.com>
2023-10-15 12:23:44 +0530

    [feat]: implementing load balancing using eureka-client-discovery

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/client/CurrencyExchangeClient.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/client/CurrencyExchangeClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "http://localhost:8000")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeClient
 {
     @GetMapping(value = GlobalConstants.API + "/currency-exchange/from/{from}/to/{to}")
