72462cca71d86826150afe2a538cbc13fe6238b5
Sanket Mhetre <sanket.mhetre@neptune-ubi.com>
2023-10-15 11:05:02 +0530

    [feat]: added global constants file

---
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/business/common/GlobalConstants.java
@@ -0,0 +1,15 @@
+package com.in28minutes.microservices.currencyconversionservice.business.common;
+
+public class GlobalConstants
+{
+    // api
+    public static final String API = "/api";
+
+    // properties
+    public static final String LOCAL_SERVER_PORT = "local.server.port";
+
+    private GlobalConstants()
+    {
+
+    }
+}
